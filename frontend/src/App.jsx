e99b9247b18650e9a45638506283b8f0513b348c
Felipe <fleipeds@gmail.com>
2026-02-11 17:43:54 -0400

    Teste de Husky

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,17 +2,40 @@ import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "@/app/router/AppRoutes";
 import { QueryProvider } from "@/app/providers/QueryProvider";
 import { MuiThemeProvider } from "@/app/providers/ThemeProvider";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    console.log("Ta rodando")
+  })
+  
   return (
     <QueryProvider>
+
+
+
+    
+
+
+
+
+
+    {}
       <MuiThemeProvider>
         <BrowserRouter>
-          <main className="min-h-screen bg-gray-50">
+          <main className="min-h-screen bg-gray-50" form='' alvo="">
             <AppRoutes />
           </main>
         </BrowserRouter>
-      </MuiThemeProvider>
+     
+     
+     
+     
+     
+     
+     
+     
+                                                               </MuiThemeProvider>
     </QueryProvider>
   );
 }
